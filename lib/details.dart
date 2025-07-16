457309a7731b97982e70b083b2dccd9c609a423d
anjanakrishnan7 <anjanakrishnananil@gmail.com>
2025-07-16 16:02:40 +0530

    finished

--- lib/details.dart
+++ lib/details.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:multiscreen_quote_app/model.dart';
 class Details extends StatelessWidget {
   const Details({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Container(),),);
+    final Quote details=ModalRoute.of(context)!.settings.arguments as Quote;
+    return Scaffold(
+      body: Center(
+        child: Container(width: 450,height: 450,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.white,Colors.blue])
+          ),
+          child: Column(
+            children: [
+              Text("${details.quote}",style: GoogleFonts.nunitoSans(fontSize: 40),),
+              Text("${details.author}",style: GoogleFonts.aboreto(fontSize: 20),),
+
+            ]
+          ),
+        ),
+      ),
+    );
   }
 }
